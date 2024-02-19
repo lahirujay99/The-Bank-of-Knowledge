a03cbda6bdd0bf36e21bfd995f7a4c981e312929
lahirujay99 <lahirujayawardana94@gmail.com>
2024-02-19 09:10:06 +0530

    view courses coponent done

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,15 @@
-import {
-  Card,
-  Box,
-  Container,
-  Flex,
-  Grid,
-  Text,
-  Avatar,
-} from "@radix-ui/themes";
+import { Avatar, Box, Container, Flex, Grid, Text } from "@radix-ui/themes";
 import Image from "next/image";
 import Link from "next/link";
 import { MdOutlineKeyboardArrowRight } from "react-icons/md";
-import { TbCircleNumber1 } from "react-icons/tb";
 
 export default function Home() {
   return (
     <div>
       <Hero />
       <DiscoverOurTraining />
+      <TheValueBring />
+      <CoursesList />
     </div>
   );
 }
@@ -179,5 +172,137 @@ const DiscoverOurTraining = () => {
         </Grid>
       </Grid>
     </Container>
+  );
+};
+
+const TheValueBring = () => {
+  return (
+    <div className="bg-[#D6EDFF]">
+      <Container>
+        <Grid
+          columns={{ initial: "1", md: "3" }}
+          className="bg-[#D6EDFF] mb-[50px]"
+        >
+          <Flex
+            direction="column"
+            align="start"
+            className="mt-[60px] p-5 md:col-span-2"
+            gap="4"
+          >
+            <p className="font-bold text-[#1D4596] text-[48px] ">
+              The Value It Brings You
+            </p>
+            <p className="font-normal text-[#1B1B1B] text-[17px] ">
+              Unlocking a world of possibilities for your personal and
+              professional development to pave the way for a resilient future of
+              unparalleled success by investing in our training packages and
+              workshops, delivered through a variety of different training
+              methods to suit your learning style.
+            </p>
+            <p className="font-normal text-[#1B1B1B] text-[17px] ">
+              Unlocking a world of possibilities for your personal and
+              professional development to pave the way for a resilient future of
+              unparalleled success by investing in our training packages and
+              workshops, delivered through a variety of different training
+              methods to suit your learning style.
+            </p>
+            <button className="inline-flex items-center bg-[#F18F02]  text-white font-medium py-2 px-4 rounded text-[18px]">
+              Learn More
+              <span className="mr-1"></span>
+              <MdOutlineKeyboardArrowRight />
+            </button>
+          </Flex>
+          <Box className="pt-[60px] pb-5">
+            <Image
+              src="/Office Meeting.png"
+              alt="hero image"
+              width={409}
+              height={466}
+            />
+          </Box>
+        </Grid>
+      </Container>
+    </div>
+  );
+};
+
+const CoursesList = () => {
+  const courses = [
+    {
+      courseType: "Traning Course",
+      courseName: "Knowledge Bank CPT",
+      courseDesc:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's",
+      courseLink: "/courses/id",
+      enquire: "course/enquare/id",
+    },
+    {
+      courseType: "Traning Course",
+      courseName: "Sales and Service",
+      courseDesc:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's",
+      courseLink: "/courses/id",
+      enquire: "course/enquare/id",
+    },
+    {
+      courseType: "Traning Course",
+      courseName: "Hybrid Working",
+      courseDesc:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's",
+      courseLink: "/courses/id",
+      enquire: "course/enquare/id",
+    },
+    {
+      courseType: "Traning Course",
+      courseName: "Well-being in the Workplace",
+      courseDesc:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's",
+      courseLink: "/courses/id",
+      enquire: "course/enquire/id",
+    },
+  ];
+  return (
+    <div>
+      <Container>
+        <Grid
+          columns={{ initial: "1", md: "4" }}
+          className="pt-[70px] pb-[30px]"
+          gap="5"
+        >
+          {courses.map((course) => (
+            <Flex
+              direction="column"
+              key={course.courseLink}
+              className="bg-[#1C4596] text-white pt-5 pl-5 pr-5 "
+              gap="4"
+            >
+              <p className="font-medium text-[16px]">{course.courseType}</p>
+              <p className="font-bold text-[20px]">{course.courseName}</p>
+              <p className="font-normal text-[16px]">{course.courseDesc}</p>
+              <Flex justify="between" className="border-t mt-5 pt-5">
+                <Link
+                  href={course.courseLink}
+                  className="text-[#F18F02] inline-flex items-center font-medium text-[18px] "
+                >
+                  View Course
+                  <MdOutlineKeyboardArrowRight />
+                </Link>
+                <Link
+                  href={course.courseLink}
+                  className="text-[#F18F02]  font-medium text-[18px]"
+                >
+                  Enquire
+                </Link>
+              </Flex>
+            </Flex>
+          ))}
+        </Grid>
+        <Flex justify="center" className="pt-[20px] py-[70px]">
+          <button className="inline-flex items-center bg-[#F18F02]  text-white font-medium text-[18px] py-2 px-4 rounded">
+            View All Courses
+          </button>
+        </Flex>
+      </Container>
+    </div>
   );
 };
